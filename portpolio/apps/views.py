fca13f854de66306a0ce103ea130cb2816d6f17a
YuMinHa <qhahd78>
2020-08-11 19:52:13 +0900

    스터디 포트폴리오

---
+++ portpolio/apps/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request) :
+    return render(request, 'home.html')
+
+def detail(request):
+    return render(request, 'detail.html')
